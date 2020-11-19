1c163f2768a9277565df1568d8e9c080986aa6ed
david-masa <60571920+david-masa@users.noreply.github.com>
2020-11-19 17:44:58 +0900

    編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,18 @@ class PostsController < ApplicationController
     @comments = @post.comments.includes(:user)
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(params[:id]), notice: '投稿を編集しました'
+    else
+      render :edit
+    end
+  end
 
   def destroy
     post = Post.find(params[:id])
