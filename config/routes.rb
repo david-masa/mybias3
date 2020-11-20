ba187f5f804cecd33274f88fa53c38a4ec0f7fc0
david-masa <60571920+david-masa@users.noreply.github.com>
2020-11-21 06:22:56 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update, :destroy]
   resources :kpop, only: :index
   resources :jpop, only: :index
+  resources :thaipop, only: :index
 end
