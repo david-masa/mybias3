e668c244f7a50a32c400dfa52439807112f733d6
david-masa <60571920+david-masa@users.noreply.github.com>
2020-11-23 17:11:50 +0900

    ビュー仮作成

--- app/controllers/goodranking_controller.rb
+++ app/controllers/goodranking_controller.rb
@@ -1,5 +1,6 @@
 class GoodrankingController < ApplicationController
   def index
     @goodrankings = Post.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
+    
   end
 end
