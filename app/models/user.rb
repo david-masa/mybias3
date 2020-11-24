7d7fb01aa093389079f5a9256c204890a64ed336
david-masa <60571920+david-masa@users.noreply.github.com>
2020-11-24 12:01:57 +0900

    ルーティング、モデル、コントローラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,14 @@ class User < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
+
     def already_liked?(post)
       self.likes.exists?(post_id: post.id)
     end
