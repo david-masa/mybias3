179714f652f94c06a18ea73016263e73dce41dff
david-masa <60571920+david-masa@users.noreply.github.com>
2020-11-25 08:34:00 +0900

    仮ファイル

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+
+    it "nameがない場合は登録できないこと" do
+      user = User.new(name: nil, email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+    
+    it "emailがない場合は登録できないこと" do
+      user = User.new(name: "Alexa", email: nil, password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+  end
+
+
+end
