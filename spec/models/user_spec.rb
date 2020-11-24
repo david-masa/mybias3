56c6148ba015717b262a03d141989e6f7f4d41ed
david-masa <60571920+david-masa@users.noreply.github.com>
2020-11-25 08:59:59 +0900

    factory_botの記法の省略factory_botの記法の省略

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ describe User do
   describe '#create' do
 
     it "nameがない場合は登録できないこと" do
-      user = User.new(name: nil, email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build()
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
