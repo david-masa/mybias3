865f0da1dec0cd74dd836afc7ef1f72b3ed4587c
david-masa <60571920+david-masa@users.noreply.github.com>
2020-11-25 09:38:26 +0900

    userテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,66 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
+    it "nameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
     it "nameがない場合は登録できないこと" do
-      user = build()
+      user = build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
+
+    it " 重複したnameが存在する場合は登録できないこと " do
+      user = create(:user)
+      another_user = build(:user, name: user.name)
+      another_user.valid?
+      expect(another_user.errors[:name]).to include("はすでに存在します")
+    end
     
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordが6文字に満たない場合は登録できないこと" do
+      user = build(:user, password: "aaaaa")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上で入力してください")
+    end
+
+    it " passwordが6文字以上であれば登録できること " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      expect(user).to be_valid
+    end
+    
+    it "password_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
     it "emailがない場合は登録できないこと" do
-      user = User.new(name: "Alexa", email: nil, password: "00000000", password_confirmation: "00000000")
+      user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
+
+    it " 重複したemailが存在する場合は登録できないこと " do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+    
+    it "imageがない場合も登録できること" do
+      user = build(:user, image: nil)
+      user.valid?
+      expect(user).to be_valid
+    end
+
 
   end
 
