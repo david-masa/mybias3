e1ec29ce4ed412da7eec7e34fb2365d5d2478a52
david-masa <60571920+david-masa@users.noreply.github.com>
2020-11-29 15:50:53 +0900

    youtube_url,英数字のみの入力

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :reason, presence: true, length: { maximum: 255 }
   validates :youtube_url, presence: true
+  validates_format_of :youtube_url, with: /\A[a-zA-Z0-9]+\z/
   validates :category_id, presence: true
   enum category_id: { KPOP: 1, JPOP: 2, ThaiPOP: 3, ChinesePOP: 4, RussianPOP: 5, IndianPOP: 6 }
 
