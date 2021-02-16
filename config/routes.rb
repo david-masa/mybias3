c99e5a468615ec3cf7b91b8c45de4d3747917415
david-masa <60571920+david-masa@users.noreply.github.com>
2021-02-16 19:09:43 +0900

    ルーティング、ヘッダーに紐付け

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :indianpop, only: :index
   resources :russianpop, only: :index
   resources :goodranking, only: :index
+  resources :search, only: :index
   
 end
